958e0c7d78e1b1c2e436c4dc08f9d9f9244e85b6
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-05 12:46:01 -1000

    all CRUD functions working

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -4,7 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodosController extends Controller
 {
-    //
+    public function index(){
+
+        $todos = Todo::all();
+
+        return view('todos')->with('todos', $todos);
+    }
+
+    public function store(Request $request){
+
+        $todo = new Todo;
+
+        $todo->todo = $request->todo;
+
+        $todo->save();
+
+        return redirect()->back();
+
+    }
+
+    public function delete($id){
+        $todo = Todo::find($id);
+
+        $todo->delete();
+
+        return redirect()->back();
+
+    }
+
+    public function update($id){
+        $todo = Todo::find($id);
+        return view('update')->with('todo', $todo);
+    }
+
+    public function save(Request $request, $id){
+        $todo = Todo::find($id);
+
+        $todo->todo = $request->todo;
+        $todo->save();
+
+        return redirect()->route('todos');
+
+    }
 }
